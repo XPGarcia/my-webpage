3c5f71cf8f8e0ca48fc48bf9931def5edb4a611e
Xavier García <xavier.garcia@prometeo.dev>
2023-02-26 10:27:09 -0500

    Images updated with lower sized

--- global/projects.ts
+++ global/projects.ts
@@ -64,7 +64,7 @@ export const projects: Record<ProjectKey, Project> = {
       'Developed multiple RESTful API utilizing Domain-Driven Design (DDD) and Hexagonal Architecture, resulting in a highly maintainable and scalable system that emphasizes business rules and separation of concerns',
       'Use of microservices for better separation of concerns and the ability to easily maintain and update individual components of the system. Microservices with Docker containers to create a highly scalable and flexible system that can easily handle increased traffic and usage',
       'Developed multiple RESTful APIs using the NestJS framework, leveraging its powerful features such as dependency injection, modules, and decorators to create highly maintainable and testable code. Implemented secure authentication using API keys. Use of a custom error handling library in all microservices, resulting in a standardized and consistent approach to error handling across the entire application',
-      '"Development of the e-commerce CMS using Angular for dynamic components and real-time updates, and Tailwind CSS for rapid styling of UI components with minimal CSS code'
+      'Development of the e-commerce CMS using Angular for dynamic components and real-time updates, and Tailwind CSS for rapid styling of UI components with minimal CSS code'
     ],
     skills: [
       skills.typescript,
